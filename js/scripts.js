1cafd4f77b86f92c68a6c67d585f4fa378e23b48
William Chu <williamchu51@gmail.com>
2018-12-23 09:17:58 -0800

    Add experimental parallax

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,49 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  // Parallax
+  $(document).ready(function(){
+    $(window).scroll(function(e){
+      parallax();
+    });
+    function parallax(){
+      var scrolled = $(window).scrollTop();
+      $('.parallax').css('top',-(scrolled*0.0315)+'rem');
+      $('.parallax > .intro-body').css('top',-(scrolled*-0.005)+'rem');
+      $('.parallax > .intro-body').css('opacity',1-(scrolled*.00175));
+    };
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 48
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
+  $("body").scrollspy({
+    target: "#mainNav",
     offset: 54
   });
 
@@ -38,5 +59,4 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
 })(jQuery); // End of use strict
